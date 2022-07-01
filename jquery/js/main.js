5711b15299c74ed6590e550f6cb6fcc712a1bba6
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-01 22:48:09 +0500

    each this

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
-    $('.mainText').before("<span>Перед объектом </span>");
-    $('.mainText').after("<span>После объектом </span>");
-    $('.mainText').append("<span> В конце внутри</span>");
-    $('.mainText').prepend("<span> 123 </span>");
+    $('img').each(function(){
+        
+        if($(this).attr('src') == 'img/logo.png'){
+            $(this).addClass('each');
+        }
+    });
 });
