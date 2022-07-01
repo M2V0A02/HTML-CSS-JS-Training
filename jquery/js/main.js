a6457f3c42e7b725f7a5ed2fa7f464df5068a032
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-01 22:34:58 +0500

    animate

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
-    $('img').css({
-        'width' : '20px',
-        'height' : '20px'
-    });
-    alert($('img').css([ "color", "font-size"])['font-size']);
+    $('a').animate({
+        'font-size': "24px",
+        "color": "rgb( 20, 20, 20 )",
+        'line-height':"200%"
+    }, 3000)
 });
