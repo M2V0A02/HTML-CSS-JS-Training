63c3288bd2d07d24d6625c94daa102ec4838df61
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-01 23:08:18 +0500

    mouserover mouseout click mousemove mouseup

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,4 +1,19 @@
 $(function(){
-    $('img, a').css('background', 'black');
-    $('img[alt]').css('background', 'red');
+    $('img, a').mouseover(function(){
+        console.log('mouserOver');
+    });
+    $('img, a').mouseout(function(){
+        console.log('mouserOut');
+    });
+    $('img, a').click(function(){
+        console.log('mouserclick');
+    });
+    $('img, a').mousemove(function(){
+        console.log('mousermove');
+    });
+    $('img, a').mouseup(function(){
+        console.log('mouserUp');
+    })
+
+    scroll(console.log('qwe'));
 });
