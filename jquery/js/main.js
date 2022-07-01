7befd6bc4df75d2f3a1805a51e776ed6b6040f9c
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-01 21:08:41 +0500

    html

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,6 +1,5 @@
 $(function(){
     let tagP = $('.mainText > p').hide(1000).show(2000);
     $("li").hide(500).show(1000);
-    alert($('.form1').width());
-    alert($('.form1').height())
+    $("li").html("<b>li</b>");
 });
