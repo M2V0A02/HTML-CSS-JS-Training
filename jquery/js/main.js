9e0002e589d8ad320cf51bd5184e8707a48db3c0
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-01 21:32:09 +0500

    fadiiN fadeOut

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-    let tagP = $('.mainText > p').hide(1000).show(2000);
-    $("li").hide(500).show(1000);
-    $("li").html("<b>li</b>");
+    $("nav").hide(1000).show(1000);
+    $(".mainText").fadeOut(1000).fadeIn(1000);
 });
