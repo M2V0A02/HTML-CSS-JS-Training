ec3df783163d43045a167cf1190c69159e4d73d1
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-08 17:31:16 +0500

    Сделал слайдер

--- bem-2/js/main.js
+++ bem-2/js/main.js
@@ -4,3 +4,39 @@ $('.horizontal-charts__item').each(function(){
     $(this).find('.horizontal-charts__line').append('<div style="width:' + percent * widthParent / 100 + 'px; height: 2px; background-color: #1D1D1D; position: absolute"> </div>');
     $(this).find('.horizontal-charts__percent').css('left', (widthParent * percent / 100 - 9) + 'px');
 });
+
+let controlPanel = $('.slider__control-panel');
+for (let i = 0; i < $('.slider__item').length; i++){
+    let appendToControlPanel = document.createElement("div");
+    appendToControlPanel.className = "slider__page";
+    appendToControlPanel.setAttribute('page', i);
+    if (i == 0 ){
+        appendToControlPanel.className += " slider__page--active";
+    }
+    controlPanel.append(appendToControlPanel);
+} 
+
+$('.slider__page').click(function(){
+    $('.slider__item').css('display', 'none');
+    $('.slider__item').eq($(this).attr('page')).css('display', 'inline-block');
+    $('.slider__page').removeClass('slider__page--active');
+    $(this).addClass('slider__page--active');
+});
+
+$('.slider__forward').click(function(){
+    if ($('.slider__page').last().attr('page') == $('.slider__page--active').attr('page'))
+        return
+    let nextSliderItem = $('.slider__page--active').removeClass('slider__page--active').next();
+    nextSliderItem.addClass('slider__page--active');
+    $('.slider__item').css('display', 'none');
+    $('.slider__item').eq(nextSliderItem.attr('page')).css('display', 'inline-block');
+});
+
+$('.slider__back').click(function(){
+    if ($('.slider__page').first().attr('page') == $('.slider__page--active').attr('page'))
+        return 
+    let nextSliderItem = $('.slider__page--active').removeClass('slider__page--active').prev();
+    nextSliderItem.addClass('slider__page--active');
+    $('.slider__item').css('display', 'none');
+    $('.slider__item').eq(nextSliderItem.attr('page')).css('display', 'inline-block');
+});
