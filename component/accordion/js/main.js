5665dc0ce2fc04398110bbe0bedfea821809ffe1
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-16 00:31:32 +0500

    Сделал аккордион

---
+++ component/accordion/js/main.js
@@ -0,0 +1,8 @@
+$('.accordion__item').click(function() {
+    $('.accordion__slider').css({'animation':'rotate-0 1s forwards'});
+    $('.accordion__header').css({'background':'#fff', 'border': 'none'});
+    $('.accordion__content').slideUp(500);
+    $(this).find('.accordion__slider').css({'animation':'rotate-180 1s forwards'});
+    $(this).find('.accordion__header').css({'background':'#909090', 'border':'3px solid black'});
+    $(this).find('.accordion__content').slideDown(1000);
+});
