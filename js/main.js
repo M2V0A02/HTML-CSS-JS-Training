099e5e1fa6605930d0ec5f896d93397469fe471e
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-24 12:26:51 +0500

    Контекст

--- js/main.js
+++ js/main.js
@@ -153,3 +153,52 @@ for (var i = 0; i < 5; i++) {
 result[1]()
 result[2]()
 result[4]()
+
+const person = {
+    surname: 'Старк',
+    knows: function (what, name) {
+        console.log(`Ты ${what} знаешь, ${name} ${this.surname}`)
+    }
+}
+
+const john = {surname: 'Сноу' }
+
+person.knows('все', 'Бран');
+person.knows.call(john, 'ничего не', 'Джон')
+person.knows.apply(john, ['ничего не', 'Джон'])
+person.knows.call(john, ...['ничего не', 'Джон'])
+person.knows.bind(john, ...['ничего не', 'Джон'])()
+
+function Person(name, age) {
+    this.name = name
+    this.age = age
+
+    console.log(this)
+}
+
+const elena = new Person('Elena', 20)
+function logThis() {
+    console.log(this)
+}
+
+const obj = {num:42}
+logThis.apply(obj)
+logThis.call(obj)
+logThis.bind(obj)()
+
+const animal = {
+    legs: 4,
+    logThis: function() {
+        console.log(this)
+    }
+}
+
+animal.logThis()
+
+function Cat(color) {
+    this.color = color
+    console.log('This', this);
+    ( () => console.log('Arrow this', this))()
+}
+
+new Cat('red')
