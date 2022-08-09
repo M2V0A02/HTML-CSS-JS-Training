2bfef27d5aff78b3995221aca8d61e83d3d4c642
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-09 12:56:12 +0500

    fetch GEt

---
+++ xhr/fetch.js
@@ -0,0 +1,16 @@
+const requestURL = "https://jsonplaceholder.typicode.com/users"
+
+function sendRequest(method, url, body = null) {
+    return fetch(url).then(response => {
+         return response.json()
+    })
+}
+
+sendRequest('GET', requestURL)
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+
+const body = {
+    name: 'Vladilen',
+    age: 26
+}
